f2ef4d6581ba2e9307f31b0d5d91c63e81618fd6
greeneley <anhchangtoanhoc97@gmail.com>
2025-01-05 14:56:58 +0700

    routing vs service nestjs

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthService } from './auth/auth.service';
 
-@Controller('user')
+export class BodyDto {
+	name: string;
+	age: number;
+}
+
+@Controller('users')
 export class UserController {
 	constructor(
 		private readonly userService: UserService,
@@ -12,7 +17,21 @@ export class UserController {
 	}
 
 	@Get() // /user
-	index() {
-		return [this.userService.getUsers(), this.authService.login()];
+	index(@Query() query: any) {
+		// return [this.userService.getUsers(), this.authService.login()];
+		return {
+			keyword: query.keyword
+		};
+	}
+
+	@Post() // -> body
+	createWithBody(@Body() bodyDto: BodyDto) {
+		console.log({ bodyDto });
+		return 'post create' + JSON.stringify(bodyDto);
+	}
+
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return `This action returns a ${id} cat`;
 	}
 }
